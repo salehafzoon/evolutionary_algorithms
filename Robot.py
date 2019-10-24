f56e518e4019c1bab1e3e63b00ab4a65b0f87c4b
saleh afzoon <saleh.afzoon.76@gmail.com>
2019-10-24 20:27:47 +0330

    create Individual class in robot problem

--- Robot.py
+++ Robot.py
@@ -8,209 +8,199 @@ found = False
 population = []
 strg_fitness = 0
 
-POPULATION_SIZE = 4
-MAX_NUMBER = 10
+POPULATION_SIZE = 5000
+MAX_NUMBER = 243
 ACTION_LIST = ["n","s","e","w","r","st","b"]
+# ACTION_LIST = ["n","s","e","w","b"]
 GRID_WIDTH = 10
-CURR_LOC = (0,0)
-CAN_POS = [(0, 0), (1, 1), (2, 2), (3, 3), 
-          (4, 4), (5, 5), (6, 6), (7, 7),
-          (8, 8), (9, 9), (10, 10)]
+CURR_LOC = (5,5)
+CAN_POS = []
 
 METHOD = "onePoint"
-
-
-def create_gnome():
-    gene = []
-    
-    for i in range(1, MAX_NUMBER + 1):
+  
+class Individual(object):
+      
+    def __init__(self, chromosome): 
+        self.chromosome = chromosome  
+        self.fitness = self.call_fitness()
+  
+    @classmethod
+    def create_gnome(self): 
         
-        action = random.choice(list(ACTION_LIST))
-        gene.append(action)
-
-    return gene
-    
-def call_fitness(gene):
-    global CURR_LOC
-    fitness = 0
-    
-    (x,y) = CURR_LOC
-    for action in gene :
+        gene = []
+        for i in range(MAX_NUMBER):    
+            action = random.choice(list(ACTION_LIST))
+            gene.append(action)
             
-        if action =="st":
-            x=x
-        elif action =="n":
-            y +=1
-        elif action =="s":
-            y-=1
-        elif action == "e":
-            x+=1
-        elif action == "w":
-            x-=1
-        elif action == "b" and CURR_LOC in CAN_POS:
-            CAN_POS.remove(CURR_LOC)
-            fitness +=10
+        return gene
+  
+     
+    @classmethod
+    def rouletteWheelSelection(self,population):
         
-        elif action == "r":
-            r = 0
-            dec_inc = random.random()
-            if dec_inc>0.5:
-                r = 1
-            else:
-                r = -1
-                    
-            hor_or_ver = random.random()
-            if(hor_or_ver > 0.5):
-                x +=r
-            else:
-                y +=r
+        fitness_sum = 0
+        parents = []
+        min_fit = -1000
+        fitnesses = []
         
-        # print ("\n")
-        # print("before",CURR_LOC)
+        for individual in population:
+            fitnesses.append(individual.fitness)
         
-        # check if hit the wall
-        if x >10 or x<0 or y>10 or y<0:
-            fitness -=5
-            x = max(0,x)
-            y = max(0,x)
-            
-        
-        CURR_LOC = (x,y)
-        
-        # print("action:",action,"fitness :",fitness,"after",CURR_LOC)
-         
-    return fitness
+        min_fit = abs(min(fitnesses))
 
-def crossOver(parent1, parent2):
-    child = []
-    if METHOD == "uniform":
-        for c1 , c2 in zip(parent1 , parent2):
-            prob = random.random()
-            if(prob > 0.5):
-                child.append(c1)
-            else:
-                child.append(c2)
-    
-    if METHOD == "onePoint":
-        size = len(parent1)
-        child = parent1[:size/2]
-        for i in range(size/2,size):
-            child.append(parent2[i])
-        
-    return child
-        
-def mutate(child):
-    chrom = random.choice(child)
-    index = child.index(chrom)
-    
-    action = random.choice(list(ACTION_LIST))
-    child[index] = action
-            
-    return child
-    
-def rouletteWheelSelection(population):
-    
-    fitness_sum = 0
-    parents = []
-    min_fit = -1000
-    fitnesses = []
-    
-    
-    for individual in population:
-        fitnesses.append(call_fitness(individual))
-    
-    min_fit = abs(min(fitnesses))
-
-    print("fitnesses",fitnesses,"fitness_sum",fitness_sum)
-    
-    for i in range(len(fitnesses)):
-        fitnesses[i] += min_fit +10
-        
-    fitness_sum = sum(fitnesses)
-    
-    print("fitnesses",fitnesses,"fitness_sum",fitness_sum)
-    
-    filled = 0
-        
-    temp = []
-    for i in range(len(fitnesses)) :
-        
-        fitnesses[i] =  float(filled + fitnesses[i] /fitness_sum)  
-        temp.append(fitnesses[i])
-        filled = fitnesses[i]
-    
-        
-    print("fitnesses",fitnesses)
-    print("temp",temp)
-    
-    for j in range(2):
-        pointer = random.random()
-        
-        print("pointer",pointer)
+        # print("fitnesses",fitnesses)
         
         for i in range(len(fitnesses)):
+            fitnesses[i] += min_fit +10
             
-            if pointer < fitnesses[i]:
-                print("selected",population[i])
-                parents.append(population[i])
-                break
+        fitness_sum = sum(fitnesses)
+        
+        # print("fitnesses",fitnesses,"fitness_sum",fitness_sum)
+        
+        filled = 0
+            
+        for i in range(len(fitnesses)) :
+            
+            fitnesses[i] = float(filled + fitnesses[i] /float(fitness_sum))
+            filled = fitnesses[i]
+        
+            
+        # print("fitnesses",fitnesses)
+        # print("population",population)
+        
+        for j in range(2):
+            pointer = random.random()
+            
+            # print("pointer",pointer)
+            
+            for i in range(len(fitnesses)):
                 
-    return parents
+                if pointer < fitnesses[i]:
+                    # print("selected",population[i])
+                    parents.append(population[i])
+                    break
+                    
+        return parents
+
+    def mutate(self):
+        for chrom in self.chromosome:
+            mutateProb = random.random()
+            if(mutateProb <= 0.005):
+                index = self.chromosome.index(chrom)
+                action = random.choice(list(ACTION_LIST))
+                self.chromosome[index] = action
+        
+    def crossOver(self, parent2): 
+
+        child = []
+        if METHOD == "uniform":
+            for c1 , c2 in zip(self.chromosome , parent2.chromosome):
+                prob = random.random()
+                if(prob > 0.5):
+                    child.append(c1)
+                else:
+                    child.append(c2)
+        
+        if METHOD == "onePoint":
+            size = len(self.chromosome)
+            child = self.chromosome[:size/2]
+            for i in range(size/2,size):
+                child.append(parent2.chromosome[i])
+            
+        return Individual(child)
+
+    def call_fitness(self): 
+        
+        global CURR_LOC
+        fitness = 0
+        (x,y) = CURR_LOC
+        can_pos = CAN_POS[:]
+        
+        for action in self.chromosome :
+            
+            if action =="st":
+                x=x
+            elif action =="n":
+                y +=1
+            elif action =="s":
+                y-=1
+            elif action == "e":
+                x+=1
+            elif action == "w":
+                x-=1
+            elif action == "b" and CURR_LOC in can_pos:
+                can_pos.remove(CURR_LOC)
+                fitness +=10
+            
+            elif action == "r":
+                r = 0
+                dec_inc = random.random()
+                if dec_inc>0.5:
+                    r = 1
+                else:
+                    r = -1
+                        
+                hor_or_ver = random.random()
+                if(hor_or_ver > 0.5):
+                    x +=r
+                else:
+                    y +=r
+            
+            # check if hit the wall
+            if x >10 or x<0 or y>10 or y<0:
+                fitness -=5
+                x = max(0,x)
+                y = max(0,x)
+                
+            
+            CURR_LOC = (x,y)
+                    
+        return fitness 
 
 if __name__ == '__main__':
-
-
-    for i in range(4):
-        population.append(create_gnome())
-
-    parents = rouletteWheelSelection(population)
     
-             
-    # print("First Position : ",CURR_LOC)
-    # # First Generation
-    # for i in range(0, POPULATION_SIZE):
-    #     population.append(create_gnome())
-
-    # population[0] = ['b', 'r', 'r', 's', 'b', 'e', 'b', 'n', 'b', 'w','b','r','b']
-    # print(population[0])
+    for i in range(10):
+        for j in range(10):
+            CAN_POS.append((i,j))  
     
-    # print("total fitness:",call_fitness(population[0]))
-    
-    
-    # while not found:
+    # First Generation    
+    for _ in range(POPULATION_SIZE): 
+        gnome = Individual.create_gnome() 
+        indiv = Individual(gnome)
+        if indiv not in population:
+            population.append(indiv) 
 
-    #     population = sorted(population, key=lambda gene:call_fitness(gene))
 
-    #     print("generation:",generation," best fit:",call_fitness(population[0]))
+    while not found:
         
-    #     if call_fitness(population[0]) <= 0:
-    #         found = True
-    #         break
+        population = sorted(population, reverse = True,key = lambda x:x.fitness)
         
-    #     new_generation = []
+        print("generation:",generation," best fit:",population[0].fitness)
         
-    #     index = int(POPULATION_SIZE *0.4)
+        if population[0].fitness == len(CAN_POS)* 10:
+            found = True
+            break
         
-    #     for _ in range(POPULATION_SIZE):
+        new_generation = []
+        
+        index = int(POPULATION_SIZE *0.7)
             
-    #         (parent1 , parent2) = rouletteWheelSelection(population)
-    #         child = crossOver(parent1,parent2)
+        for _ in range(POPULATION_SIZE):
             
-    #         mutateProb = random.random()
-    #         if(mutateProb <= 0.5):
-    #             mutate(child)
-                
-    #         new_generation.append(child) 
+            (parent1 , parent2) = Individual.rouletteWheelSelection(population[index:])
+            child = parent1.crossOver(parent2)
+            
+            child.mutate()
+            
+            new_generation.append(child) 
                 
   
-    #     population = new_generation 
-    #     generation += 1
+        population = new_generation 
+        generation += 1
 
 
-    # print("generation->",generation,"       ",population[0] ,  call_fitness(population[0]))
+    print("generation->",generation,"       ",population[0].chromosome ,  population[0].fitness)
 
-    # duration = time.time() - start
-    # print ("minute:",(duration)//60)
-    # print("second:" ,(duration)%60)
-
-        
+    duration = time.time() - start
+    print ("minute:",(duration)//60)
+    print("second:" ,(duration)%60)
