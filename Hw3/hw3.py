092b111ad6b2d992c9d2bdc4cfbb159c49ba53ec
saleh afzoon <saleh.afzoon.76@gmail.com>
2019-12-30 12:33:20 +0330

    non adaptive sigma added

--- Hw3/hw3.py
+++ Hw3/hw3.py
@@ -4,58 +4,64 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 from random import gauss
+import numpy as np
 
 MAX_VAL = 500
 CASE1 = "CASE1"
 CASE2 = "CASE2"
+NON_ADAPTIVE = "NON_ADAPTIVE"
+
 LOCAL_INT = "LOCAL_INT"
 GLOBAL_INT = "GLOBAL_INT"
 LOCAL_DISC = "LOCAL_DISC"
 GLOBAL_DISC = "GLOBAL_DISC"
+ELITISM = "ELITISM"
+GENERATIONAL = "GENERATIONAL"
 
 generation = 1
 population = []
 found = False
 
-MAX_GENERATION = 400
-POPULATION_SIZE = 400
-N = 3
+MAX_GENERATION = 50
+POPULATION_SIZE = 100
+N = 2
 ALPHA = 418.9829
- 
+
 MUTATION_MODE = CASE2
 XOVER_METHOD = LOCAL_DISC
-
-def truncate(f, n):
-    '''Truncates/pads a float f to n decimal places without rounding'''
-    s = '{}'.format(f)
-    if 'e' in s or 'E' in s:
-        return '{0:.{1}f}'.format(f, n)
-    i, p, d = s.partition('.')
-    return '.'.join([i, (d+'0'*n)[:n]])
-
+SURVIVOR_SEL_TYPE = GENERATIONAL
+sigArray = []
 class Individual(object):
 
     def __init__(self, chromosome):
+            
         self.chromosome = chromosome
         (self.f_array,self.fitness) = self.call_fitness()
+        self.valid = True
+    @classmethod
+    def initConstantSigmas(self):
+        for _ in range(N):
+            sigArray.append(random.random())
 
     @classmethod
     def create_chromosome(self):
         x = []
         sig = []
-        
+            
         # init object variables
         for _ in range(N):
             x.append( random.randrange(-500,500) )
-            # x.append(420.9687)
         
         # init random sigma from (0,1)
-        if MUTATION_MODE == 'CASE1' :
+        if MUTATION_MODE == CASE1:
             sig.append(random.random())
-        elif MUTATION_MODE == 'CASE2' :
+        elif MUTATION_MODE == CASE2:
             for _ in range(N):
                 sig.append(random.random())
+        elif MUTATION_MODE == NON_ADAPTIVE:
+            sig.extend(sigArray)
 
+        # print("sig:",sig)
         chromosome = (x,sig)
         return chromosome
 
@@ -65,35 +71,33 @@ class Individual(object):
 
         # calcute t*N(0,1)
         t = (1/math.sqrt(2*N))
+        tCor = (1/math.sqrt( 2*math.sqrt(N)) )
         const = t * gauss(0,1)
 
-        if(MUTATION_MODE == CASE2) :
+        if(MUTATION_MODE == CASE1) :
+            sig[0] *= math.exp(const)
+        elif (MUTATION_MODE == CASE2) :
             for i in range(len(sig)):
-                var = t * gauss(0,1)
+                var = tCor * gauss(0,1)
                 sig[i] *= math.exp(const + var)
-            for i in range(len(x)):
-                # print("sig:",sig[i])
-                # print("sig* n:",sig[i] * gauss(0,1))
-
-                # print("x[i] before:",x[i])
-                x[i] += sig[i] * gauss(0,1)
-                if x[i] > 500 :
-                    x[i] = 500
-                if x[i] < 0 :
-                    x[i]= 0
-                # print("x[i]:",x[i])
-                # x[i] = float(truncate(x[i], 4))
-                x[i] = round(x[i], 4)
-                
-
-        # change our chromosome after mutate
-        self.chromosome = (x,sig)
         
-        # print("before:",self.fitness)
-        # calculate fitness again
-        (self.f_array,self.fitness) = self.call_fitness()
-        # print("after:",self.fitness)
-
+        for i in range(len(x)):
+            if(MUTATION_MODE == CASE1) :
+                #  x[i] =  round( x[i] + sig[0] * gauss(0,1) ,7)
+                 x[i] += sig[0] * gauss(0,1)
+                 
+            elif MUTATION_MODE == CASE2 or MUTATION_MODE == NON_ADAPTIVE:
+                # x[i] =  round( x[i] + sig[i] * gauss(0,1) ,7)
+                x[i] += sig[i] * gauss(0,1) 
+            
+            if x[i] >= 500 or x[i]<-500:
+                self.valid = False
+        
+        # change our chromosome after mutate
+        if(self.valid):
+            self.chromosome = (x,sig)
+            (self.f_array,self.fitness) = self.call_fitness()
+        
     @classmethod
     def crossOver(self, population):
 
@@ -138,9 +142,13 @@ class Individual(object):
                 
                 x1 = parent1.chromosome[0][i]
                 x2 = parent2.chromosome[0][i]
-
-                sig1 = parent1.chromosome[1][i]
-                sig2 = parent2.chromosome[1][i]
+                
+                if MUTATION_MODE == CASE2:
+                    sig1 = parent1.chromosome[1][i]
+                    sig2 = parent2.chromosome[1][i]
+                else:
+                    sig1 = parent1.chromosome[1][0]
+                    sig2 = parent2.chromosome[1][0]
 
                 if XOVER_METHOD == GLOBAL_DISC:
                     child_x.append(random.choice([x1,x2]))
@@ -161,44 +169,40 @@ class Individual(object):
         f_array = []
         fitness = 0
         x = self.chromosome[0]
+        
         for i in range ( len(x)):
             # f_array.append( round(- x[i]*math.sin(math.sqrt(math.fabs(x[i]))) ,10))
             f_array.append(- x[i]*math.sin(math.sqrt(math.fabs(x[i]))))
-    
+
         for f in f_array :
             fitness +=f
 
-        fitness = round(float(fitness) + ALPHA*len(x),8)
+        fitness = float(fitness) + ALPHA*len(x)
 
         return (f_array , fitness)
 
 def initial_population():
+    population = []
     for _ in range(POPULATION_SIZE):
         chrom = Individual.create_chromosome()
         indiv = Individual(chrom)
         population.append(indiv)
+    return population
 
-
-if __name__ == '__main__':
-
-    # fitness = 0
-    # f_array = []
-    # x = [404.97441816,-858.9798881]
-    # for i in range ( len(x)):
-    #     f_array.append(- x[i]*math.sin(math.sqrt(math.fabs(x[i]))))
-
-    # for f in f_array :
-    #     fitness +=f
-
-    # fitness = float(fitness) + ALPHA*len(x)
-
-    # print(fitness)
-
-    start = time.time()
+def ga():
 
     # First Generation
-    initial_population()
- 
+    global generation
+    global population
+
+    if MUTATION_MODE == NON_ADAPTIVE :
+        Individual.initConstantSigmas()
+        
+    population = initial_population()
+    
+    found = False
+    generation = 0
+
     while not found:
 
         # max generation terminate condition
@@ -207,10 +211,9 @@ if __name__ == '__main__':
         
         population = sorted(population, key=lambda x: x.fitness)
         
-        # for ind in population[0:3]:
-        #     print(ind.fitness,ind.chromosome[0])
-        # print("------pop--------")
+        # print("generation:",generation,population[0].fitness,population[0].chromosome[0][0:3])
         
+        # print(len(population))
         if population[0].fitness == 0 :
             break
         
@@ -221,26 +224,45 @@ if __name__ == '__main__':
             child = Individual.crossOver(population)
             for _ in range(7):
                 child.mutate()
-                new_generation.append(child)
+                if(child.valid) :
+                    new_generation.append(child)
         
+        if(SURVIVOR_SEL_TYPE == ELITISM):
+            new_generation.extend(population)
+
         new_generation = sorted(
             new_generation, key=lambda x: x.fitness)
-    
-        # for ind in new_generation[0:3]:
-        #     print(ind.fitness,ind.chromosome[0])
-        # print("------new gen--------")
 
         population = new_generation[0:POPULATION_SIZE]
 
-        # for ind in new_generation[0:3]:
-        #     print(ind.fitness,ind.chromosome[0])
-        # print("------new pop--------")
-
         generation += 1
 
-    print("generation :", generation, " x =",
-          population[0].chromosome[0], " f =",population[0].fitness)
+    # print(population[0].fitness,population[0].f_array,"\n")
+    print(population[0].fitness)
+    return population[0].fitness
 
+if __name__ == '__main__':
 
-    duration = time.time() - start
-    print("time :", (duration)//60 ,"min,",round((duration) % 60 , 4) ,"sec")
+    start = time.time()
+
+    fitnesses = []
+
+    for _ in range(10):
+
+        fitnesses.append( ga() )
+
+    print("fitnesses:",fitnesses)
+
+    print("average:",np.mean(fitnesses))
+    print("variance:",math.sqrt(np.var(fitnesses)))
+
+    # print("generation :", generation, " x =",
+    #       population[0].chromosome[0], " fitness =",population[0].fitness)
+    
+    # for i in range(len(population[0].f_array)):
+    #     population[0].f_array[i] += ALPHA
+    
+    # print("f_array:", population[0].f_array)
+
+    # duration = time.time() - start
+    # print("time :", (duration)//60 ,"min,",round((duration) % 60 , 4) ,"sec")
